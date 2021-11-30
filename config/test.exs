769ecc783a11d7a3a09cc81ec10ddffa9330b281
Kamil Zieliński <paryzowy@gmail.com>
2021-11-30 12:19:39 +0100

    mix test on docker db

--- config/test.exs
+++ config/test.exs
@@ -7,9 +7,10 @@ import Config
 # Run `mix help test` for more information.
 config :rush_hour, RushHour.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "mysecretpassword",
   database: "rush_hour_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
+  port: 5433,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
